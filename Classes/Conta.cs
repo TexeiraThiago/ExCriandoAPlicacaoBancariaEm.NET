9af5cc08fce5421d20fd9b70f340b8324649a933
Thiago Teixeira <thiagot@id.uff.br>
2021-10-24 15:41:42 -0300

    Cria classe Conta

---
+++ Classes/Conta.cs
@@ -0,0 +1,20 @@
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private string Nome { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private TipoConta TipoConta { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+    }
+
+}
